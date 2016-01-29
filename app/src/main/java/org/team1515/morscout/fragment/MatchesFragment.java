77262c0c993e053233ab28c680942b19bcfcc718
ariellas <ariella.simoi@gmail.com>
2016-01-28 17:00:33 -0800

    Pages need implimentation. Settings xml configured.

--- app/src/main/java/org/team1515/morscout/fragment/MatchesFragment.java
+++ app/src/main/java/org/team1515/morscout/fragment/MatchesFragment.java
@@ -1,9 +1,20 @@
 package org.team1515.morscout.fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.team1515.morscout.R;
 
 /**
  * Created by prozwood on 1/25/16.
  */
 public class MatchesFragment extends Fragment {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_matches, container, false);
+
+            return view;
+        }
 }
--- app/src/main/java/org/team1515/morscout/fragment/TeamFragment.java
+++ app/src/main/java/org/team1515/morscout/fragment/TeamFragment.java
@@ -1,9 +1,20 @@
 package org.team1515.morscout.fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.team1515.morscout.R;
 
 /**
  * Created by prozwood on 1/25/16.
  */
 public class TeamFragment extends Fragment {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_team, container, false);
+
+            return view;
+        }
 }
