bebacffae83a5f23a9cfcc1aa5ee3a4aef31e52a
Preston Rozwood <prozwood@wks-hs-191-08.bhusd.k12.ca.us>
2016-01-31 13:56:12 -0800

    Design improvements

--- app/src/main/java/org/team1515/morscout/fragment/HomeFragment.java
+++ app/src/main/java/org/team1515/morscout/fragment/HomeFragment.java
@@ -1,6 +1,7 @@
 package org.team1515.morscout.fragment;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -22,8 +23,29 @@ import org.team1515.morscout.R;
  * Created by prozwood on 1/25/16.
  */
 public class HomeFragment extends Fragment {
+    TextView title;
+    TextView message;
+
+    Typeface Exo2_thin;
+    Typeface Exo2_light;
+
+    String[] msgs = {"Welcome to MorScout!", "OurScout is MorScout Than YourScout", "Made With Fifty Shades of Orange", "MorPower, MorTeamwork, MorIngenuity, MorScout", "LessWork, MorScout"};
+    int randIndex;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        randIndex = (int) Math.floor(Math.random() * msgs.length);
+
+        Exo2_thin = Typeface.createFromAsset(getActivity().getAssets(), "Exo2-Thin.ttf");
+        Exo2_light = Typeface.createFromAsset(getActivity().getAssets(), "Exo2-Light.ttf");
+
+        title = (TextView) view.findViewById(R.id.home_title);
+        title.setTypeface(Exo2_light);
+
+        message = (TextView) view.findViewById(R.id.home_message);
+        message.setTypeface(Exo2_thin);
+        message.setText(msgs[randIndex]);
 
         return view;
     }
