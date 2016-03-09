d15f1ecb30758fc2abd238911e091f0923e36bcd
DaPasta <484902@gmail.com>
2016-03-08 19:15:38 -0800

    Add title to team activity.

--- app/src/main/java/org/team1515/morscout/activity/TeamActivity.java
+++ app/src/main/java/org/team1515/morscout/activity/TeamActivity.java
@@ -14,11 +14,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.team1515.morscout.R;
 import org.team1515.morscout.adapter.TeamPagerAdapter;
+import org.team1515.morscout.network.CookieRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TeamActivity extends AppCompatActivity {
     SharedPreferences preferences;
@@ -58,6 +67,42 @@ public class TeamActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.team_tabLayout);
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+        getTeamInfo();
+    }
+
+    private void getTeamInfo() {
+        Map<String, String> params = new HashMap<>();
+        params.put("teamNumber", String.valueOf(team));
+
+        CookieRequest teamRequest = new CookieRequest(Request.Method.POST,
+                "/getTeamInfo",
+                params,
+                preferences,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println(response);
+                        try {
+                            JSONObject teamObj = new JSONObject(response);
+                            String name = "Team " + team + " - " + teamObj.getString("nickname");
+
+                            TextView nameView = (TextView) findViewById(R.id.team_name);
+                            nameView.setText(name);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        queue.add(teamRequest);
     }
 
 }
