d61bbb6313760ed172b30ca9369e79c11c8d1bf3
DaPasta <484902@gmail.com>
2016-03-14 16:41:20 -0700

    Fix title width on weird screen resolutions.

--- app/src/main/java/org/team1515/morscout/fragment/main/HomeFragment.java
+++ app/src/main/java/org/team1515/morscout/fragment/main/HomeFragment.java
@@ -3,6 +3,7 @@ package org.team1515.morscout.fragment.main;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,14 +12,16 @@ import android.widget.TextView;
 import org.team1515.morscout.R;
 
 public class HomeFragment extends Fragment {
-    TextView title;
-    TextView message;
+    private int titleWidth = 70;
 
-    Typeface Exo2_thin;
-    Typeface Exo2_light;
+    private TextView title;
+    private TextView message;
 
-    String[] msgs = {"Welcome to MorScout!", "OurScout is MorScout Than YourScout", "Made With Fifty Shades of Orange", "MorPower, MorTeamwork, MorIngenuity, MorScout", "LessWork, MorScout"};
-    int randIndex;
+    private Typeface Exo2_thin;
+    private Typeface Exo2_light;
+
+    private String[] msgs = {"Welcome to MorScout!", "OurScout is MorScout Than YourScout", "Made With Fifty Shades of Orange", "MorPower, MorTeamwork, MorIngenuity, MorScout", "LessWork, MorScout"};
+    private int randIndex;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
@@ -34,6 +37,14 @@ public class HomeFragment extends Fragment {
         message = (TextView) view.findViewById(R.id.home_message);
         message.setTypeface(Exo2_thin);
         message.setText(msgs[randIndex]);
+
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        float screenWidth = displayMetrics.widthPixels;
+        title.measure(0, 0);
+        while(title.getMeasuredWidth() > screenWidth - 20) {
+            title.measure(0, 0);
+            title.setTextSize(--titleWidth);
+        }
 
         return view;
     }
