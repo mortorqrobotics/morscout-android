e6504cdde9b1254bd14888f4952da59bb0e501d0
DaPasta <484902@gmail.com>
2016-03-25 18:06:44 -0700

    Add all information to profile page

--- app/src/main/java/org/team1515/morscout/fragment/main/ProfileFragment.java
+++ app/src/main/java/org/team1515/morscout/fragment/main/ProfileFragment.java
@@ -1,11 +1,20 @@
 package org.team1515.morscout.fragment.main;
 
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -23,6 +32,8 @@ import org.json.JSONObject;
 import org.team1515.morscout.R;
 import org.team1515.morscout.network.CacheRequestQueue;
 import org.team1515.morscout.network.CookieRequest;
+import org.team1515.morscout.network.ImageCookieRequest;
+import org.w3c.dom.Text;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,16 +41,17 @@ import java.util.Map;
 public class ProfileFragment extends Fragment {
     private RequestQueue queue;
     private SharedPreferences preferences;
-    private ImageLoader imageLoader;
+//    private ImageLoader imageLoader;
 
-    NetworkImageView picture;
-    TextView matchReports;
-    TextView pitReports;
-    TextView uniqueTeams;
-    TextView matchesComplete;
-    TextView matchesIncomplete;
-    TextView matchCompletion;
-
+    private ImageView picture;
+    private TextView matchReports;
+    private TextView pitReports;
+    private TextView uniqueTeams;
+    private TextView matchesComplete;
+    private TextView matchesIncomplete;
+    private TextView matchCompletion;
+    private TextView assignments;
+    private CheckBox scoutCaptain;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
@@ -47,13 +59,15 @@ public class ProfileFragment extends Fragment {
         queue = Volley.newRequestQueue(getContext());
         preferences = getContext().getSharedPreferences(null, 0);
 
-        picture = (NetworkImageView) view.findViewById(R.id.profile_picture);
+        picture = (ImageView) view.findViewById(R.id.profile_picture);
         matchReports = (TextView) view.findViewById(R.id.profile_matchReports);
         pitReports = (TextView) view.findViewById(R.id.profile_pitReports);
         uniqueTeams = (TextView) view.findViewById(R.id.profile_uniqueTeams);
         matchesComplete = (TextView) view.findViewById(R.id.profile_matchesComplete);
         matchesIncomplete = (TextView) view.findViewById(R.id.profile_matchesIncomplete);
         matchCompletion = (TextView) view.findViewById(R.id.profile_matchCompletion);
+        assignments = (TextView) view.findViewById(R.id.profile_assignments);
+        scoutCaptain = (CheckBox) view.findViewById(R.id.profile_captain);
 
         getProfile();
 
@@ -61,18 +75,31 @@ public class ProfileFragment extends Fragment {
     }
 
     private void getProfile() {
-        imageLoader = CacheRequestQueue.getInstance(getContext()).getImageLoader();
-        final String url = "http://www.morteam.com" + preferences.getString("picPath", "") + "-300";
-        System.out.println(url);
-        imageLoader.get(url, ImageLoader.getImageListener(picture,
-                android.R.drawable.ic_dialog_alert, android.R.drawable
-                        .ic_dialog_alert));
-        picture.setImageUrl(url, imageLoader);
+//        imageLoader = CacheRequestQueue.getInstance(getContext()).getImageLoader();
+//        final String url = "http://www.morteam.com" + preferences.getString("picPath", "") + "-300";
+//        System.out.println(url);
+//        imageLoader.get(url, ImageLoader.getImageListener(picture,
+//                android.R.drawable.ic_dialog_alert, android.R.drawable
+//                        .ic_dialog_alert));
+//        picture.setImageUrl(url, imageLoader);
+        ImageCookieRequest pictureRequest = new ImageCookieRequest("http://www.morteam.com" + preferences.getString("picPath", "") + "-300",
+                preferences,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        picture.setImageBitmap(response);
+                    }
+                }, 0, 0, null, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        queue.add(pictureRequest);
 
         Map<String, String> params = new HashMap<>();
-//        params.put("scoutID", preferences.getString("userId", ""));
-        params.put("userID", "56500985d218a24520d4ac9a");
-        params.put("scoutID", "56500985d218a24520d4ac9a");
+        params.put("scoutID", preferences.getString("userId", ""));
+        params.put("userID", preferences.getString("userId", ""));
 
         CookieRequest statsRequest = new CookieRequest(Request.Method.POST,
                 "/getUserStats",
@@ -108,6 +135,7 @@ public class ProfileFragment extends Fragment {
                     @Override
                     public void onResponse(String response) {
                         try {
+                            System.out.println(response);
                             JSONObject tasksObj = new JSONObject(response);
 
                             JSONArray incompleteMatches = tasksObj.getJSONArray("matchesNotDone");
@@ -131,6 +159,28 @@ public class ProfileFragment extends Fragment {
                             matchesComplete.setText(completeMatchesStr);
 
                             matchCompletion.setText(completeMatches.length() + "/" + (incompleteMatches.length() + completeMatches.length()));
+
+
+                            JSONArray assignmentArray = tasksObj.getJSONArray("assignments");
+                            if(assignmentArray.length() == 0) {
+                                assignments.setText("None");
+                            } else {
+                                SpannableStringBuilder assignmentBuilder = new SpannableStringBuilder();
+                                for (int i = 0; i < assignmentArray.length(); i++) {
+                                    JSONObject assignmentObj = assignmentArray.getJSONObject(i);
+                                    SpannableString assignmentMatches = new SpannableString("From Match "
+                                            + assignmentObj.getString("startMatch") + " to " + assignmentObj.getString("endMatch")
+                                            + ": ");
+                                    assignmentMatches.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, assignmentMatches.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                                    SpannableString assignmentTeams = new SpannableString(assignmentObj.getString("alliance").substring(0, 1).toUpperCase() + assignmentObj.getString("alliance").substring(1)
+                                            + " Alliance, Team " + assignmentObj.getString("teamSection"));
+
+                                    assignmentBuilder.append(assignmentMatches);
+                                    assignmentBuilder.append(assignmentTeams);
+                                    assignmentBuilder.append("\n\n");
+                                }
+                                assignments.setText(assignmentBuilder);
+                            }
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
